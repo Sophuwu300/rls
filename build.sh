049682e366207f5cd0c246c40338e09bf0d93c2d
sophuwu <sophie@sophuwu.site>
2023-08-05 19:25:33 +0200

    made buildsh better

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cmake -S . -B build -GNinja
-cmake --build build
-chmod +x build
-strip build/rls
-build/rls $1
-
+cmake -S . -B build -GNinja && \
+cmake --build build && \
+chmod +x build/rls && \
+strip --strip-all build/rls && \
+upx --best build/rls
